0764d701e84fd3cef449e8397acd6fa7405a3a8a
ballboy <ballboyst365@gmail.com>
2025-07-13 23:15:41 +0900

    create api directory

--- next.config.ts
+++ next.config.ts
@@ -3,5 +3,14 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
 };
-
+module.exports = {
+  async rewrites(){
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'http://host.docker .internal:8000/api/:path*/',
+      },
+    ]
+  },
+};
 export default nextConfig;
